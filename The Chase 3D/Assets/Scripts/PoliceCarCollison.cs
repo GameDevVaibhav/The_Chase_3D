26821449109ab7fb565e8aa4f0b825036f9ac274
GameDevVaibhav <147718788+GameDevVaibhav@users.noreply.github.com>
2023-12-19 13:42:54 +0530

    Police Healthbar added

--- The Chase 3D/Assets/Scripts/PoliceCarCollison.cs
+++ The Chase 3D/Assets/Scripts/PoliceCarCollison.cs
@@ -5,13 +5,33 @@ using UnityEngine;
 public class PoliceCarCollison : MonoBehaviour
 {
     public GameObject explosionPrefab; // Drag your explosion prefab here
+    private float collisionTimer = 0f;
+    private bool isCollidingWithPoliceCar = false;
+    private float currentHealth;
+    private float maxHealth = 4f;
+
+    // Reference to the PoliceHealthBar script
+    [SerializeField] private PoliceHealthBar healthBar;  // Assuming you have a reference to the PoliceHealthBar
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.UpdateHealthBar(maxHealth,currentHealth);
+    }
 
     void OnCollisionEnter(Collision collision)
     {
         bool playerCollision = collision.gameObject.CompareTag("Player");
         bool obstacleCollision = collision.gameObject.CompareTag("Obstacle");
+        bool policeCarCollision = collision.gameObject.CompareTag("PoliceCar");
 
-        if (playerCollision || obstacleCollision)
+        if (policeCarCollision)
+        {
+            isCollidingWithPoliceCar = true;
+        }
+
+
+        if ((playerCollision || obstacleCollision) && !isCollidingWithPoliceCar)
         {
             InstantiateExplosion();
             AudioManager.Instance.PlayExplosionSound(); // Access the AudioManager to play the explosion sound
@@ -19,12 +39,41 @@ public class PoliceCarCollison : MonoBehaviour
         }
     }
 
+    void OnCollisionExit(Collision collision)
+    {
+        bool policeCarCollision = collision.gameObject.CompareTag("PoliceCar");
+
+        if (policeCarCollision)
+        {
+            isCollidingWithPoliceCar = false;
+        }
+    }
+
+    void Update()
+    {
+        if (isCollidingWithPoliceCar)
+        {
+            collisionTimer += Time.deltaTime;
+            currentHealth = maxHealth - collisionTimer;
+            healthBar.UpdateHealthBar(maxHealth,currentHealth);
+            // Check if the collision has lasted for more than 4 seconds
+            if (collisionTimer > 4f)
+            {
+                InstantiateExplosion();
+                AudioManager.Instance.PlayExplosionSound(); // Access the AudioManager to play the explosion sound
+                Destroy(gameObject);
+            }
+
+            
+        }
+    }
+
     void InstantiateExplosion()
     {
         if (explosionPrefab != null)
         {
-            GameObject explosion=Instantiate(explosionPrefab, transform.position, Quaternion.identity);
-            Destroy(explosion, 5f);
+            GameObject explosion = Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(explosion, 4f);
         }
     }
 
---
+++ The Chase 3D/Assets/Scripts/PoliceHealthBar.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PoliceHealthBar : MonoBehaviour
+{
+    
+    public Image healthBar;
+    private Camera camera;
+
+    private void Start()
+    {
+        camera= Camera.main;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+        transform.rotation = Quaternion.LookRotation(transform.position - camera.transform.position);
+    }
+    public void  UpdateHealthBar(float maxHealth,float currentHealth)
+    {
+       healthBar.fillAmount = currentHealth/maxHealth;
+    }
+}
