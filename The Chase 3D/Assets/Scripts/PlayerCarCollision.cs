1036092da7a5cd4c400e2ac30f560992b39de49e
GameDevVaibhav <147718788+GameDevVaibhav@users.noreply.github.com>
2023-12-19 15:35:11 +0530

    Player Health Bar added

--- The Chase 3D/Assets/Scripts/PlayerCarCollision.cs
+++ The Chase 3D/Assets/Scripts/PlayerCarCollision.cs
@@ -4,20 +4,29 @@ using UnityEngine;
 
 public class PlayerCarCollision : MonoBehaviour
 {
-    public AudioSource explosionSfx;
+    public float maxHealth = 100f;
+    public float currentHealth;
+
+    [SerializeField] private PlayerHealthBar healthBar;
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHealth = maxHealth;
+        healthBar.UpdateHealthBar(maxHealth, currentHealth);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        healthBar.UpdateHealthBar(maxHealth, currentHealth);
     }
     private void OnCollisionEnter(Collision collision)
     {
-      
+        bool policeCarCollision = collision.gameObject.CompareTag("PoliceCar");
+        if(policeCarCollision )
+        {
+            currentHealth -= 10f;
+            
+        }
     }
 }
---
+++ The Chase 3D/Assets/Scripts/PlayerHealthBar.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthBar : MonoBehaviour
+{
+    public Image healthBar;
+    float lerpSpeed;
+
+    private void Update()
+    {
+        lerpSpeed= 3f*Time.deltaTime;
+    }
+    public void UpdateHealthBar(float maxHealth, float currentHealth)
+    {
+        healthBar.fillAmount = Mathf.Lerp(healthBar.fillAmount, currentHealth/maxHealth, lerpSpeed);
+        Color healthColor = Color.Lerp(Color.red, Color.green, (currentHealth / maxHealth));
+        healthBar.color = healthColor;  
+    }
+    void ColorChange()
+    {
+        
+    }
+}
