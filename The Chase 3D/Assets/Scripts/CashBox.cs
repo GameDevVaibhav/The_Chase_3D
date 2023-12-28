34043dd3ac622f002d484d3636ceafbd51b1dfa2
GameDevVaibhav <147718788+GameDevVaibhav@users.noreply.github.com>
2023-12-28 23:45:42 +0530

    CashBox emission added
    Emission intensity increases with every shot

--- The Chase 3D/Assets/Scripts/CashBox.cs
+++ The Chase 3D/Assets/Scripts/CashBox.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CashBox : MonoBehaviour
 {
     public GameObject dollarBillParticles; // Reference to the dollar bill particle system
+    public Material cashBoxMaterial; // Reference to the material of the CashBox
+    public float emissionIncreaseRate = 0.5f; // Adjust the rate at which emission intensity increases
 
     private int hitCount = 0;
     private Score scoreManager;
@@ -13,6 +15,16 @@ public class CashBox : MonoBehaviour
     {
         // Find the Score script or adjust accordingly based on your setup
         scoreManager = FindObjectOfType<Score>();
+
+        // Ensure that a material is assigned to the CashBox
+        if (cashBoxMaterial == null)
+        {
+            Renderer renderer = GetComponent<Renderer>();
+            if (renderer != null)
+            {
+                cashBoxMaterial = renderer.material;
+            }
+        }
     }
 
     void OnTriggerEnter(Collider other)
@@ -22,6 +34,21 @@ public class CashBox : MonoBehaviour
             hitCount++;
 
             Debug.Log("CashBox Hit! Hit Count: " + hitCount);
+
+            // Increase emission intensity with every hit
+            if (cashBoxMaterial != null)
+            {
+                Color currentEmissionColor = cashBoxMaterial.GetColor("_EmissionColor");
+
+                // Assuming your emission color is green, you can modify the green channel
+                float newIntensity = currentEmissionColor.g + hitCount * emissionIncreaseRate;
+
+                // Make sure the intensity does not exceed 1
+                newIntensity = Mathf.Clamp01(newIntensity);
+
+                Color newEmissionColor = new Color(currentEmissionColor.r, newIntensity, currentEmissionColor.b);
+                cashBoxMaterial.SetColor("_EmissionColor", newEmissionColor);
+            }
 
             if (hitCount >= 5)
             {
@@ -44,6 +71,4 @@ public class CashBox : MonoBehaviour
         Debug.Log("CashBox Destroyed!");
         Destroy(gameObject);
     }
-
-
 }
